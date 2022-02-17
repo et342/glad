0fcfa524a1a7424d9f8063cf588ee82a08518afd
et342 <34111964+et342@users.noreply.github.com>
2022-02-17 07:23:05 +0500

    rust: allow cheaper proc loading

--- glad/generator/rust/templates/impl.rs
+++ glad/generator/rust/templates/impl.rs
@@ -3,9 +3,41 @@ pub use self::types::*;
 pub use self::enumerations::*;
 pub use self::functions::*;
 
-use std::os::raw::c_void;
+use std::ffi::CStr;
+use std::os::raw::{c_char, c_void};
 
 {% set ctx_name = feature_set.name | capitalize %}
+
+/// A cheap convenience wrapper around [`CStr`](std::ffi::CStr).
+#[derive(Debug)]
+#[repr(transparent)]
+pub struct FnName(CStr);
+
+impl FnName {
+    /// Returns a pointer to a NUL-terminated string.
+    #[inline]
+    pub fn as_ptr(&self) -> *const c_char {
+        self.0.as_ptr()
+    }
+
+    /// Returns a string without trailing NUL byte.
+    #[inline]
+    pub fn as_str(&self) -> &str {
+        unsafe {
+            std::str::from_utf8_unchecked(self.0.to_bytes())
+        }
+    }
+
+    #[inline]
+    pub fn as_c_str(&self) -> &CStr {
+        &self.0
+    }
+
+    #[inline]
+    unsafe fn from_bytes_with_nul_unchecked(bytes: &[u8]) -> &Self {
+        std::mem::transmute(CStr::from_bytes_with_nul_unchecked(bytes))
+    }
+}
 
 #[derive(Copy, Clone)]
 struct FnPtr {
@@ -26,7 +58,7 @@ impl FnPtr {
     fn set_ptr(&mut self, ptr: *const c_void) {
         *self = Self::new(ptr);
     }
-    
+
     fn aliased(&mut self, other: &FnPtr) {
         if !self.is_loaded && other.is_loaded {
             *self = *other;
@@ -112,12 +144,14 @@ mod storage {
 {% endif %}
 
 {% if options.mx %}
-pub fn load<F>(mut loadfn: F) -> functions::{{ ctx_name }} where F: FnMut(&'static str) -> *const c_void {
+pub fn load<F>(mut loadfn: F) -> functions::{{ ctx_name }} where F: FnMut(&'static FnName) -> *const c_void {
     #[allow(unused_mut)]
-    let mut ctx = {{ ctx_name }} {
-        {% for command in feature_set.commands %}
-        {{ template_utils.protect(command.name) }} {{ command.name|no_prefix }}: FnPtr::new(loadfn("{{ command.name }}")),
-        {% endfor %}
+    let mut ctx = unsafe {
+        {{ ctx_name }} {
+            {% for command in feature_set.commands %}
+            {{ template_utils.protect(command.name) }} {{ command.name|no_prefix }}: FnPtr::new(loadfn(FnName::from_bytes_with_nul_unchecked(b"{{ command.name }}\0"))),
+            {% endfor %}
+        }
     };
 
     {% for command, caliases in aliases|dictsort %}
@@ -129,10 +163,10 @@ pub fn load<F>(mut loadfn: F) -> functions::{{ ctx_name }} where F: FnMut(&'stat
      ctx
 }
 {% else %}
-pub fn load<F>(mut loadfn: F) where F: FnMut(&'static str) -> *const c_void {
+pub fn load<F>(mut loadfn: F) where F: FnMut(&'static FnName) -> *const c_void {
     unsafe {
         {% for command in feature_set.commands %}
-        {{ template_utils.protect(command) }} storage::{{ command.name | no_prefix }}.set_ptr(loadfn("{{ command.name }}"));
+        {{ template_utils.protect(command) }} storage::{{ command.name | no_prefix }}.set_ptr(loadfn(FnName::from_bytes_with_nul_unchecked(b"{{ command.name }}\0")));
         {% endfor %}
 
         {% for command, caliases in aliases|dictsort %}
@@ -143,4 +177,3 @@ pub fn load<F>(mut loadfn: F) where F: FnMut(&'static str) -> *const c_void {
     }
 }
 {% endif %}
-
